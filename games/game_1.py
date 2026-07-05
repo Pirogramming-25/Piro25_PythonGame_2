9e531c23ce1ce24691af863550751ee27f4064f7
borissal1207 <borissal127@gmail.com>
2026-07-05 16:00:28 +0900

    feat: 홍삼게임 체인 지목, 더블 지목 로직 구현

--- games/game_1.py
+++ games/game_1.py
@@ -16,14 +16,14 @@ def play(current_player, others):
     targets = random.sample(candidates, 2)
     print(f'{current_player}(이)가 외친다 ! 아싸 너너 ! {targets[0]}, {targets[1]}')
 
-    pending = targets
+    pending = targets # pending은 다음 라운드에 반응할 사람
 
     for round in range(20):
-        count = {}
+        count = {} # 지목 개수를 저장할 딕셔너리
         for target in pending:
             if target == current_player:
                 print('당신이 지목당했습니다 ! 다음 사람을 지목하세요 !')
-                new_target = input('이름을 입력해주세요')
+                new_target = input('입력 : ')
 
                 if new_target in pool and new_target != current_player:
                     print(f'{target}이 외친다 ! 아싸 너 ! {new_target}')
@@ -33,7 +33,7 @@ def play(current_player, others):
                     return {current_player : 1}
 
             else:
-                options = []
+                options = [] # 지목 당한 사람을 제외한 사람들
                 for p in pool:
                     if p != target:
                         options.append(p)
@@ -45,6 +45,40 @@ def play(current_player, others):
                 else:
                     print(f'{target}(이)가 외친다 ! 아싸 너 ! {new_target}')
                     count[new_target] = count.get(new_target, 0) + 1
+            
+        double_target = None
+        
+        for name, cnt in count.items():
+            if cnt >= 2:
+                double_target = name
+        
+        if double_target == None:
+            pending = list(count.keys()) # 이번에 지목 당한 사람들을 다음 라운드에 반응할 사람들로 이어지게
+        else:
+            if double_target == current_player:
+                print('당신이 지목당했습니다 ! 3초 안에 "아싸 홍삼 !"을 외치세요 !')
+                answer = input('입력 : ')
+                
+                if answer == '아싸 홍삼 !':
+                    print('에브리바디 홍삼 !')
+                    return {}
+                else:
+                    print(f'{current_player}(이)는 반응하지 못했습니다 . . . 벌칙 !')
+                    return {current_player: 1}
+                
+            else:
+                if random.random() < 0.40:
+                    print(f'{double_target}(이)는 반응하지 못햇습니다 . . . 벌칙 !')
+                    return {double_target: 1}
+                else:
+                    print(f'{double_target}(이)가 외친다 ! 아싸 홍삼 !')
+                    print('에브리바디 홍삼 !')
+                    return {}
+                
+                
+
+                    
+                
                 
         
 
@@ -55,3 +89,4 @@ def play(current_player, others):
 
 if __name__== "__main__":
     play('보민', ['현민', '지연', '주헌', '희원'])
+    
